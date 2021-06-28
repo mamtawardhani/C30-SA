393112661b476b9a313613d79a499d4bcb51ad86
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-28 16:32:24 +0530

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -12,10 +12,10 @@ class CannonBall {
 
     this.image = loadImage("./assets/cannonball.png");
     this.tower = loadImage("./assets/gray.jpg");
+    this.trajectory = [];
     World.add(world, this.body);
   }
 
-  //shooting the cannonball
   shoot() {
     var velocity = p5.Vector.fromAngle(cannon.angle);
     velocity.mult(20);
@@ -26,7 +26,6 @@ class CannonBall {
   display() {
     var angle = this.body.angle;
     var pos = this.body.position;
-    console.log(pos);
     push();
     translate(pos.x, pos.y);
     rotate(angle);
@@ -34,6 +33,13 @@ class CannonBall {
     image(this.image, 0, 0, this.r, this.r);
     pop();
 
-   
+    if (this.body.velocity.x > 0 && this.body.position.x > 300) {
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+
+    for (var i = 0; i < this.trajectory.length; i++) {
+      image(this.image, this.trajectory[i][0], this.trajectory[i][1], 5, 5);
+    }
   }
 }
