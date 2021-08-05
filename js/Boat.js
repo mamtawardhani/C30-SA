91ba14e1d8ed05d2a815324ef58dca4f9d7833c7
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-08-05 16:11:09 +0530

    Update Boat.js

--- js/Boat.js
+++ js/Boat.js
@@ -1,31 +1,21 @@
 class Boat {
-  constructor(x, y, width, height, boatPos) {
-    var options = {
-      restitution: 0.8,
-      friction: 1.0,
-      density: 1.0,
-    };
 
-    this.body = Bodies.rectangle(x, y, width, height, options);
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
     this.width = width;
     this.height = height;
-
-    this.boatPosition = boatPos;
-    this.image = loadImage("assets/boat.png");
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
     World.add(world, this.body);
   }
-
-
   display() {
-    var angle = this.body.angle;
     var pos = this.body.position;
-  
     push();
     translate(pos.x, pos.y);
-    rotate(angle);
-    imageMode(CENTER);
-    image(this.image, 0, this.boatPosition, this.width, this.height);
-    noTint();
+    fill("brown");
+    rectMode(CENTER);
+    rect(0, 0, this.width, this.height);
     pop();
   }
 }
